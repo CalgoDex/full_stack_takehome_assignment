a474bd2d819b818cb2f47f0462e98eb69b8fc0e2
CalgoDex <cgg2126@columbia.edu>
2025-02-25 00:16:32 -0500

    data viewer complete

--- data-review-assignment/src/components/DataReviewTable.tsx
+++ data-review-assignment/src/components/DataReviewTable.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Tooltip as ReactTooltip } from "react-tooltip";
 import { IDataType, IDataReviewTableProps } from "../types/type-definitions";
 import { getSeverityColor, convertJsonToCsv } from "../helpers";
+import DigitalRain from "./DigitalRain";
 import ErrorModal from "./ErrorModal";
 
 const DataReviewTable: React.FC<IDataReviewTableProps> = () => {
@@ -72,212 +73,220 @@ const DataReviewTable: React.FC<IDataReviewTableProps> = () => {
   }
 
   return (
-    <div className="container" style={{ margin: "20px" }}>
-      <h1 className="text-green bg-gray-100 font-mono">Data Review</h1>
-      <div className="col">
-        {mockData && (
-          <div className="table-responsive">
-            <table key={mockData[0].id} className="table table-dark">
-              <thead className="sticky-top top-0">
-                <tr>
-                  <th scope="col">ID</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Email</th>
-                  <th scope="col">Street</th>
-                  <th scope="col">City</th>
-                  <th scope="col">Zipcode</th>
-                  <th scope="col">Phone</th>
-                  <th scope="col">Status</th>
-                  <th
-                    scope="col"
-                    data-tooltip-id={`zipcode-EM-title`}
-                    data-tooltip-content={`Zipcode Error Message`}
-                  >
-                    {`ZEM`}
-                    <ReactTooltip id={`zipcode-EM-title`} />
-                  </th>
-                  <th
-                    scope="col"
-                    data-tooltip-id={`zipcode-ES-title`}
-                    data-tooltip-content={`Zipcode Error Severity`}
-                  >
-                    ZES
-                    <ReactTooltip id={`zipcode-ES-title`} />
-                  </th>
-                  <th
-                    scope="col"
-                    data-tooltip-id={`email-EM-title`}
-                    data-tooltip-content={`Email Error Message`}
-                  >
-                    EEM
-                    <ReactTooltip id={`email-EM-title`} />
-                  </th>
-                  <th
-                    scope="col"
-                    data-tooltip-id={`email-ES-title`}
-                    data-tooltip-content={`Email Error Severity`}
-                  >
-                    EES
-                    <ReactTooltip id={`email-ES-title`} />
-                  </th>
-                  <th
-                    scope="col"
-                    data-tooltip-id={`street-EM-title`}
-                    data-tooltip-content={`Street Error Message`}
-                  >
-                    SEM
-                    <ReactTooltip id={`street-EM-title`} />
-                  </th>
-                  <th
-                    scope="col"
-                    data-tooltip-id={`street-ES-title`}
-                    data-tooltip-content={`Street Error Severity`}
-                  >
-                    SES
-                    <ReactTooltip id={`street-ES-title`} />
-                  </th>
-                  <th
-                    scope="col"
-                    data-tooltip-id={`phone-EM-title`}
-                    data-tooltip-content={`Phone Error Message`}
-                  >
-                    PEM
-                    <ReactTooltip id={`phone-EM-title`} />
-                  </th>
-                  <th
-                    scope="col"
-                    data-tooltip-id={`phone-ES-title`}
-                    data-tooltip-content={`Phone Error Severity`}
-                  >
-                    PES
-                    <ReactTooltip id={`phone-ES-title`} />
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {mockData.map((dataRow: IDataType, index) => {
-                  return (
-                    <tr
-                      key={index}
-                      onClick={() => handleRowClick(index, dataRow)}
-                      className={selectedRow === index ? "selected" : ""}
+    <div>
+      <div className="container" style={{ margin: "20px" }}>
+        <h1 style={{ justifySelf: "center", color: "white" }}>
+          <strong>Lume.ai Data Viewer</strong>
+        </h1>
+        <div className="col">
+          {mockData && (
+            <div className="table-responsive">
+              <table key={mockData[0].id} className="table table-dark">
+                <thead className="sticky-top top-0">
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Street</th>
+                    <th scope="col">City</th>
+                    <th scope="col">Zipcode</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Status</th>
+                    <th
+                      scope="col"
+                      data-tooltip-id={`zipcode-EM-title`}
+                      data-tooltip-content={`Zipcode Error Message`}
                     >
-                      <th
-                        className={getSeverityColor(dataRow?.id.toString())}
-                        scope="row"
+                      {`ZEM`}
+                      <ReactTooltip id={`zipcode-EM-title`} />
+                    </th>
+                    <th
+                      scope="col"
+                      data-tooltip-id={`zipcode-ES-title`}
+                      data-tooltip-content={`Zipcode Error Severity`}
+                    >
+                      ZES
+                      <ReactTooltip id={`zipcode-ES-title`} />
+                    </th>
+                    <th
+                      scope="col"
+                      data-tooltip-id={`email-EM-title`}
+                      data-tooltip-content={`Email Error Message`}
+                    >
+                      EEM
+                      <ReactTooltip id={`email-EM-title`} />
+                    </th>
+                    <th
+                      scope="col"
+                      data-tooltip-id={`email-ES-title`}
+                      data-tooltip-content={`Email Error Severity`}
+                    >
+                      EES
+                      <ReactTooltip id={`email-ES-title`} />
+                    </th>
+                    <th
+                      scope="col"
+                      data-tooltip-id={`street-EM-title`}
+                      data-tooltip-content={`Street Error Message`}
+                    >
+                      SEM
+                      <ReactTooltip id={`street-EM-title`} />
+                    </th>
+                    <th
+                      scope="col"
+                      data-tooltip-id={`street-ES-title`}
+                      data-tooltip-content={`Street Error Severity`}
+                    >
+                      SES
+                      <ReactTooltip id={`street-ES-title`} />
+                    </th>
+                    <th
+                      scope="col"
+                      data-tooltip-id={`phone-EM-title`}
+                      data-tooltip-content={`Phone Error Message`}
+                    >
+                      PEM
+                      <ReactTooltip id={`phone-EM-title`} />
+                    </th>
+                    <th
+                      scope="col"
+                      data-tooltip-id={`phone-ES-title`}
+                      data-tooltip-content={`Phone Error Severity`}
+                    >
+                      PES
+                      <ReactTooltip id={`phone-ES-title`} />
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {mockData.map((dataRow: IDataType, index) => {
+                    return (
+                      <tr
+                        key={index}
+                        onClick={() => handleRowClick(index, dataRow)}
+                        className={selectedRow === index ? "selected" : ""}
                       >
-                        {dataRow?.id}
-                      </th>
-                      <td
-                        className={getSeverityColor(dataRow?.name)}
-                        scope="row"
-                      >
-                        {dataRow?.name}
-                      </td>
-                      <td className={getSeverityColor(dataRow?.email)}>
-                        {dataRow?.email}
-                      </td>
-                      <td className={getSeverityColor(dataRow?.street)}>
-                        {dataRow?.street}
-                      </td>
-                      <td className={getSeverityColor(dataRow?.city)}>
-                        {dataRow?.city}
-                      </td>
-                      <td className={getSeverityColor(dataRow?.zipcode)}>
-                        {dataRow?.zipcode}
-                      </td>
-                      <td className={getSeverityColor(dataRow?.phone)}>
-                        {dataRow?.phone}
-                      </td>
-                      <td className={getSeverityColor(dataRow?.status)}>
-                        {dataRow?.status}
-                      </td>
-                      <td
-                        className={getSeverityColor(
-                          dataRow?.errors?.zipcode?.message
-                        )}
-                      >
-                        {dataRow?.errors?.zipcode?.message}
-                      </td>
-                      <td
-                        className={getSeverityColor(
-                          dataRow?.errors?.zipcode?.severity
-                        )}
-                        data-tooltip-id={`zipcode-error-${dataRow.id}`}
-                        data-tooltip-content={dataRow?.errors?.zipcode?.message}
-                      >
-                        {dataRow?.errors?.zipcode?.severity}
-                        <ReactTooltip id={`zipcode-error-${dataRow.id}`} />
-                      </td>
-                      <td
-                        className={getSeverityColor(
-                          dataRow?.errors?.email?.message
-                        )}
-                      >
-                        {dataRow?.errors?.email?.message}
-                      </td>
-                      <td
-                        className={getSeverityColor(
-                          dataRow?.errors?.email?.severity
-                        )}
-                        data-tooltip-id={`email-error-${dataRow.id}`}
-                        data-tooltip-content={dataRow?.errors?.email?.message}
-                      >
-                        {dataRow?.errors?.email?.severity}
-                        <ReactTooltip id={`email-error-${dataRow.id}`} />
-                      </td>
-                      <td
-                        className={getSeverityColor(
-                          dataRow?.errors?.street?.message
-                        )}
-                      >
-                        {dataRow?.errors?.street?.message}
-                      </td>
-                      <td
-                        className={getSeverityColor(
-                          dataRow?.errors?.street?.severity
-                        )}
-                        data-tooltip-id={`street-error-${dataRow.id}`}
-                        data-tooltip-content={dataRow?.errors?.street?.message}
-                      >
-                        {dataRow?.errors?.street?.severity}
-                        <ReactTooltip id={`street-error-${dataRow.id}`} />
-                      </td>
-                      <td
-                        className={getSeverityColor(
-                          dataRow?.errors?.phone?.message
-                        )}
-                      >
-                        {dataRow?.errors?.phone?.message}
-                      </td>
-                      <td
-                        className={getSeverityColor(
-                          dataRow?.errors?.phone?.severity
-                        )}
-                        data-tooltip-id={`phone-error-${dataRow.id}`}
-                        data-tooltip-content={dataRow?.errors?.phone?.message}
-                      >
-                        {dataRow?.errors?.phone?.severity}
-                        <ReactTooltip id={`phone-error-${dataRow.id}`} />
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        )}
+                        <th
+                          className={getSeverityColor(dataRow?.id.toString())}
+                          scope="row"
+                        >
+                          {dataRow?.id}
+                        </th>
+                        <td
+                          className={getSeverityColor(dataRow?.name)}
+                          scope="row"
+                        >
+                          {dataRow?.name}
+                        </td>
+                        <td className={getSeverityColor(dataRow?.email)}>
+                          {dataRow?.email}
+                        </td>
+                        <td className={getSeverityColor(dataRow?.street)}>
+                          {dataRow?.street}
+                        </td>
+                        <td className={getSeverityColor(dataRow?.city)}>
+                          {dataRow?.city}
+                        </td>
+                        <td className={getSeverityColor(dataRow?.zipcode)}>
+                          {dataRow?.zipcode}
+                        </td>
+                        <td className={getSeverityColor(dataRow?.phone)}>
+                          {dataRow?.phone}
+                        </td>
+                        <td className={getSeverityColor(dataRow?.status)}>
+                          {dataRow?.status}
+                        </td>
+                        <td
+                          className={getSeverityColor(
+                            dataRow?.errors?.zipcode?.message
+                          )}
+                        >
+                          {dataRow?.errors?.zipcode?.message}
+                        </td>
+                        <td
+                          className={getSeverityColor(
+                            dataRow?.errors?.zipcode?.severity
+                          )}
+                          data-tooltip-id={`zipcode-error-${dataRow.id}`}
+                          data-tooltip-content={
+                            dataRow?.errors?.zipcode?.message
+                          }
+                        >
+                          {dataRow?.errors?.zipcode?.severity}
+                          <ReactTooltip id={`zipcode-error-${dataRow.id}`} />
+                        </td>
+                        <td
+                          className={getSeverityColor(
+                            dataRow?.errors?.email?.message
+                          )}
+                        >
+                          {dataRow?.errors?.email?.message}
+                        </td>
+                        <td
+                          className={getSeverityColor(
+                            dataRow?.errors?.email?.severity
+                          )}
+                          data-tooltip-id={`email-error-${dataRow.id}`}
+                          data-tooltip-content={dataRow?.errors?.email?.message}
+                        >
+                          {dataRow?.errors?.email?.severity}
+                          <ReactTooltip id={`email-error-${dataRow.id}`} />
+                        </td>
+                        <td
+                          className={getSeverityColor(
+                            dataRow?.errors?.street?.message
+                          )}
+                        >
+                          {dataRow?.errors?.street?.message}
+                        </td>
+                        <td
+                          className={getSeverityColor(
+                            dataRow?.errors?.street?.severity
+                          )}
+                          data-tooltip-id={`street-error-${dataRow.id}`}
+                          data-tooltip-content={
+                            dataRow?.errors?.street?.message
+                          }
+                        >
+                          {dataRow?.errors?.street?.severity}
+                          <ReactTooltip id={`street-error-${dataRow.id}`} />
+                        </td>
+                        <td
+                          className={getSeverityColor(
+                            dataRow?.errors?.phone?.message
+                          )}
+                        >
+                          {dataRow?.errors?.phone?.message}
+                        </td>
+                        <td
+                          className={getSeverityColor(
+                            dataRow?.errors?.phone?.severity
+                          )}
+                          data-tooltip-id={`phone-error-${dataRow.id}`}
+                          data-tooltip-content={dataRow?.errors?.phone?.message}
+                        >
+                          {dataRow?.errors?.phone?.severity}
+                          <ReactTooltip id={`phone-error-${dataRow.id}`} />
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+        <div className="btnRow">
+          <button
+            onClick={handleExportExcel}
+            type="button"
+            className="btn btn-success"
+          >
+            Export Excel
+          </button>
+        </div>
+        <ErrorModal dataRow={selectedRowData} handleClose={handleClose} />
       </div>
-      <div className="btnRow">
-        <button
-          onClick={handleExportExcel}
-          type="button"
-          className="btn btn-success"
-        >
-          Export Excel
-        </button>
-      </div>
-      <ErrorModal dataRow={selectedRowData} handleClose={handleClose} />
     </div>
   );
 };
--- data-review-assignment/src/pages/index.tsx
+++ data-review-assignment/src/pages/index.tsx
@@ -1,12 +1,15 @@
 // pages/index.tsx
 
 import DataReviewTable from "../components/DataReviewTable";
-
+import DigitalRain from "../components/DigitalRain";
 
 export default function Home() {
   return (
-    <div className="data-visualizer-ctn">
-      <DataReviewTable />
+    <div className="data-visualizer-ctn col">
+      <DigitalRain />
+      <div className="mainDataviewer">
+        <DataReviewTable />
+      </div>
     </div>
   );
 }
